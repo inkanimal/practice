b7b17853c1e826c9e44c1d6fdca17ceed8452ed6
inkanimal <theatomproject@gmail.com>
2018-09-23 14:48:32 -0400

    models setup

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,55 +3,9 @@ require 'pry'
 
 class PlayersController < ApplicationController
 
-
-  # get '/players' do
-  #   @players = Player.all
-  #   erb :'/players/player_index'
-  # end
-  #
-  # get '/players/new' do
-  #   @positions = Position.all
-  #    @teams = Team.all
-  #   erb :'/players/new'
-  # end
-  #
-  # post '/players' do
-  #   @player = Player.create(params[:id])
-  #   # if !params[:position][:name].empty?
-  #   # player.positions.create(params[:position])
-  #   # end
-  #   if !params["team"]["name"].empty?
-  #   @player.team = Team.create(name: params["team"]["name"])
-  # end
-  #  @player.save
-  #   redirect to "players/#{@player.id}"
-  # end
-  #
-  #  get '/players/:id/edit' do
-  #   @player = Player.find_by_id(params[:id])
-  #   erb :'/players/edit'
-  # end
-  #
-  #  get '/players/:id' do
-  #   @player = Player.find_by_id(params[:id])
-  #   erb :'/players/show'
-  # end
-  #
-  # post'/players/:id' do
-  #
-  #   @player = Player.find_by_id(params[:id])
-  #   @player.update(params["player"])
-  #    if !params["team"]["name"].empty?
-  #     # binding.pry
-  #   @player.team = Team.create(name: params["team"]["name"])
-  # end
-  #   @player.save
-  #   redirect to "players/#{@player.id}"
-  # end
-
-
   get '/players' do
  @players = Player.all
+ @positions = Position.all
  erb :'players/player_index'
 end
 
